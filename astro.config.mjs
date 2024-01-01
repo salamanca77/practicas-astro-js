78108d1fbb605a0e1e02383f70b0b78058a3332d
Yersin Oliver Salamanca <laverdadteharalibre@protonmail.com>
2024-01-01 08:46:27 -0400

    modified: astro.config.mjs, packege-lock.json, packege.json, Buscador.astro -- new file: brand-youtube-filled.svg,[indice].astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
+
     adapter: netlify(),
-    output:'server'
+    // output:'server'
 });
